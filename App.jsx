078f7ae890deaea03e7951fd7ca13418d4660bd8
Lalit Joshi <91650002+lalit-hld@users.noreply.github.com>
2025-08-01 01:06:41 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LanguageToggle from './components/LanguageToggle';
+
+function App() {
+  return (
+    <div>
+      <h1>Vaishnavi Earthmovers and Transports</h1>
+      <LanguageToggle />
+    </div>
+  );
+}
+
+export default App;
